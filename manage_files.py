fd7c51a2c09a6e7408bde679b7f3107a25cab113
JJukE <psj9156@gmail.com>
2024-02-09 19:48:52 +0900

    [Mod]

--- manage_files.py
+++ manage_files.py
@@ -16,9 +16,62 @@ if __name__ == "__main__":
     parser.add_argument('--data_dir', type=str, help='Path to input directory. (Should contain the class id if you want to process the c)')
     args = parser.parse_args()
 
-    paths_temp = list(Path(args.data_dir).glob("**/*model_normalized.obj")) ## for rendering without textures
+    # paths_r = list(Path(args.data_dir).glob("**/*r_000.png"))
+    # paths_albedo = list(Path(args.data_dir).glob("**/*r_000_albedo0001.png"))
+    # paths_depth = list(Path(args.data_dir).glob("**/*r_000_depth0001.png"))
+    # paths_normal = list(Path(args.data_dir).glob("**/*r_000_normal0001.png"))
+    # print(len(paths_r))
+    # print(len(paths_albedo))
+    # print(len(paths_depth))
+    # print(len(paths_normal))
     
-    # paths = list(Path(args.data_dir).glob("**/*model_fused_20.obj"))
+    # paths_temp = list(Path(args.data_dir).glob("**/*model_normalized.obj")) ## for rendering without textures
+    # print(len(paths_temp))
+    # paths = list(Path(args.data_dir).glob("**/*model_normalized_nomtl.obj"))
+    paths = list(Path(args.data_dir).glob("**/*.npz"))
+    print(len(paths))
+
+    # # # split train, valid, test model_ids and save them.
+    # num_trains = int(len(paths) * 0.96)
+    # num_valids = (len(paths) - num_trains) // 2
+    # num_tests = len(paths) - num_trains - num_valids
+    # train_ids = []
+    # valid_ids = []
+    # test_ids = []
+    # for i, path in enumerate(paths):
+    #     if i < num_trains:
+    #         train_ids.append(path.parts[-3])
+    #     elif i < num_trains + num_valids:
+    #         valid_ids.append(path.parts[-3])
+    #     elif i < num_trains + num_valids + num_tests:
+    #         test_ids.append(path.parts[-3])
+    #     else:
+    #         print("It should not be printed.")
+    # print(len(train_ids))
+    # print(len(valid_ids))
+    # print(len(test_ids))
+    # print(len(train_ids) + len(valid_ids) + len(test_ids))
+
+    # with open("/root/hdd2/ShapeNetCoreV2/split/04379243/model_ids_train.txt", "w") as file:
+    #     for line in train_ids:
+    #         file.write(str(line) + "\n")
+    # with open("/root/hdd2/ShapeNetCoreV2/split/04379243/model_ids_valid.txt", "w") as file:
+    #     for line in valid_ids:
+    #         file.write(str(line) + "\n")
+    # with open("/root/hdd2/ShapeNetCoreV2/split/04379243/model_ids_test.txt", "w") as file:
+    #     for line in test_ids:
+    #         file.write(str(line) + "\n")
+
+    # # looking for missing renderings
+    # model_ids = []
+    # for path_r in paths_r:
+    #     model_ids.append(path_r.parts[-3])
+    
+    # print(len(model_ids))
+    # for path in paths:
+    #     if path.parts[-3] not in model_ids:
+    #         print(path.parts[-3])
+    
     # paths_ = list(Path(args.data_dir).glob("**/*volume_6M.npy"))
     
     # paths_200k = []
@@ -34,7 +87,7 @@ if __name__ == "__main__":
     # print("Number of paths containing 500K points: ", len(paths_200k))
     # print("Number of paths containing 200K points: ", len(paths_500k))
 
-    # move specific files to the destination
+    # # move specific files to the destination
     # destinations = []
     # for path in paths_:
     #     destination = Path(*path.parts[:4]) / "volume_6M" / Path(*path.parts[5:])
@@ -57,20 +110,20 @@ if __name__ == "__main__":
 
     # print("reprocess {}".format(data_paths[3056]))
 
-    # for rendering without textures
-    for path in paths_temp:
-        with open(str(path), "r") as f:
-            lines = f.readlines()
-        modified_lines = []
+    # # for rendering without textures
+    # for path in paths_temp:
+    #     with open(str(path), "r") as f:
+    #         lines = f.readlines()
+    #     modified_lines = []
 
-        for line in lines:
-            if "mtllib" not in line.lower():
-                modified_lines.append(line)
+    #     for line in lines:
+    #         if "mtllib" not in line.lower():
+    #             modified_lines.append(line)
         
-        with open(str(path.parent / "no_texture.obj"), "w") as f:
-            f.writelines(modified_lines)
+    #     with open(str(path.parent / "no_texture.obj"), "w") as f:
+    #         f.writelines(modified_lines)
         
-        print("processed {}".format(path))
+    #     print("processed {}".format(path))
     
     # print(len(paths))
     # print(len(paths_))
