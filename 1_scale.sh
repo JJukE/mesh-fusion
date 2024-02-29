547fdebb415dcd594735627f8cadda0fa4da687c
JJukE <psj9156@gmail.com>
2024-03-01 00:11:46 +0900

    [Mod] Scale and fusion code with pool + tqdm

--- 1_scale.sh
+++ 1_scale.sh
@@ -1,6 +1,6 @@
 python 1_scale.py \
-    --in_dir=/root/hdd2/ShapeNetCoreV2/ShapeNetCore.v2/04379243/ \
-    --out_dir=/root/hdd2/ShapeNetCoreV2/watertight/04379243/ \
+    --in_dir /root/hdd2/ShapeNetCoreV2/ShapeNetCore.v2/02958343/ \
+    --out_dir /root/hdd2/ShapeNetCoreV2/watertight/02958343/ \
     --padding 0.3 \
     --overwrite \
     --n_proc 16
